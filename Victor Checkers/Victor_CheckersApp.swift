065b4b81af0af5f69084d9bb76583cc89f5d7a79
Wiktor <wiktor.nizio@gmail.com>
2025-09-15 07:07:35 +0200

    Initial Commit

---
+++ Victor Checkers/Victor_CheckersApp.swift
@@ -0,0 +1,17 @@
+//
+//  Victor_CheckersApp.swift
+//  Victor Checkers
+//
+//  Created by Wiktor Nizio on 15/09/2025.
+//
+
+import SwiftUI
+
+@main
+struct Victor_CheckersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Victor CheckersTests/Victor_CheckersTests.swift
@@ -0,0 +1,17 @@
+//
+//  Victor_CheckersTests.swift
+//  Victor CheckersTests
+//
+//  Created by Wiktor Nizio on 15/09/2025.
+//
+
+import Testing
+@testable import Victor_Checkers
+
+struct Victor_CheckersTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
